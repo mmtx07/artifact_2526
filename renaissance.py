5d50b9f4b0c3c4c34208eec65768fcb47b1695c1
Máté Magyar <magyarmate07@gmail.com>
2026-04-19 22:19:18 +0200

    Add Building class and renaissance artifacts, update main to print them

---
+++ renaissance.py
@@ -0,0 +1,5 @@
+from artifact import Painting, Sculpture, Building
+
+painting = Painting("Mona Lisa", 1503, "Leonardo da Vinci", "oil")
+sculpture = Sculpture("David", 1504, "marble", 517)
+building = Building("St. Peter's Basilica", 1626, "Rome", "Renaissance")
